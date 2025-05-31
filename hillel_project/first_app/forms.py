598ee84ba2d30d482fb19c0be8d0aeb74fe74308
Viktoria Verkholat <peremoga2009@gmail.com>
2025-05-31 20:27:40 +0300

    dz6

--- hillel_project/first_app/forms.py
+++ hillel_project/first_app/forms.py
@@ -2,6 +2,7 @@ from datetime import date
 import calendar
 from django import forms
 from django.forms import ChoiceField
+from django.core.exceptions import ValidationError
 
 from first_app.models import Employee
 
@@ -14,17 +15,14 @@ class EmployeeForm(forms.ModelForm):
         fields = ('username', 'first_name', 'last_name', 'email', 'position')
 
 
-
-
 class SalaryForm(forms.Form):
-    employee = forms.ModelChoiceField(queryset=Employee.objects.all())
+    employee = forms.ModelChoiceField(queryset=Employee.objects.all(), required=True) # Додаємо required=True
 
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
         today = date.today()
-
 
         week_day, num_days = calendar.monthrange(today.year, today.month)
         for day in range(1, num_days + 1):
@@ -47,3 +45,30 @@ class SalaryForm(forms.Form):
                     choices=[(option.name, option.value) for option in WorkDayEnum],
                     initial=WorkDayEnum.WORKING_DAY.name,
                 )
+
+    def clean_employee(self):
+        employee = self.cleaned_data.get('employee')
+        if not employee:
+            raise forms.ValidationError("Будь ласка, оберіть робітника.")
+        return employee
+
+    def clean(self):
+        cleaned_data = super().clean()
+
+        sick_days_count = 0
+        holiday_days_count = 0
+
+        for field_name, value in cleaned_data.items():
+            if field_name.startswith('day_'):
+                if value == WorkDayEnum.SICK_DAY.name:
+                    sick_days_count += 1
+                elif value == WorkDayEnum.HOLIDAY.name:
+                    holiday_days_count += 1
+
+        if sick_days_count > 5:
+            self.add_error(None, forms.ValidationError("Кількість лікарняних днів не може перевищувати 5.")) # Використовуємо self.add_error для додавання загальної помилки форми
+
+        if holiday_days_count > 3:
+            self.add_error(None, forms.ValidationError("Кількість днів відпочинку не може перевищувати 3.")) # Використовуємо self.add_error для додавання загальної помилки форми
+
+        return cleaned_data
